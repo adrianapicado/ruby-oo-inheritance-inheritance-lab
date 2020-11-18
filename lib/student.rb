01f90efb06403deb519c47d4cf9e7527c6519a1e
adriana <picadoadriana7@gmail.com>
2020-11-17 20:41:01 -0500

    commit tree 28f43ff77e9d0cd3b981026fc55cfaf2081bd7be

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require 'pry'
+class Student < User 
+
+    def initialize 
+        @knowledge = []
+    end
+#binding.pry
+    def learn(string)
+        @knowledge << string
+    end
+
+    def knowledge 
+        @knowledge 
+    end
 
 end
